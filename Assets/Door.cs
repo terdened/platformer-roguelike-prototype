1ff8fd680a54f986656f29b8dbfc68754b499bf5
Denis Terekhin <terdened@mail.ru>
2018-02-11 23:48:58 +0700

    Added Room manager Added Doors

---
+++ Assets/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    public bool IsRight;
+    public bool IsLeft;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var roomManager = GameObject.Find("RoomManager").GetComponent<RoomManager>();
+
+        if (IsRight)
+            roomManager.MoveRight();
+
+        if (IsLeft)
+            roomManager.MoveLeft();
+    }
+}
---
+++ Assets/RoomManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour {
+    public GameObject[][] roomInstances;
+    public Vector2 currentPosition;
+    public GameObject camera;
+    public GameObject player;
+    public int roomWidth = 18;
+    public int roomHeight = 10;
+
+    // Use this for initialization
+    void Start () {
+        SetCurrentPosition(new Vector2(0, 0));
+    }
+
+    public void MoveRight()
+    {
+        var newPosition = this.currentPosition;
+        newPosition.x++;
+
+        SetCurrentPosition(newPosition);
+    }
+
+    public void MoveLeft()
+    {
+        var newPosition = this.currentPosition;
+        newPosition.x--;
+
+        SetCurrentPosition(newPosition);
+    }
+
+    public void SetCurrentPosition(Vector2 newPosition)
+    {
+        currentPosition = newPosition;
+
+        var roomCamera = camera.GetComponent<RoomCamera>();
+        roomCamera.SetCurrnetRoom(new Vector2(currentPosition.x, currentPosition.y));
+
+        player.transform.position = new Vector3(roomWidth * currentPosition.x, roomHeight * currentPosition.y - 2, player.transform.position.z);
+    }
+}
